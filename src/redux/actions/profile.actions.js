e253803cd5546c0adbcb9f6f42dd57ca9a3bc1b3
Trinadh Koya <trinadh.koya@mutualmobile.com>
2022-12-19 15:01:00 +0530

    saga introduced and API set up in place

---
+++ src/redux/actions/profile.actions.js
@@ -0,0 +1,24 @@
+import {FETCH_USER_INFO} from 'redux/actions/actionTypes';
+import { reduxHelper } from "redux/utils/reduxHelper";
+
+const fetchProfileRequest = () => {
+  return {
+    type: reduxHelper(FETCH_USER_INFO).actionRequest,
+  };
+};
+
+const fetchProfileSuccess = (res) => {
+  return {
+    type: reduxHelper(FETCH_USER_INFO).actionSuccess,
+    payload: res,
+  };
+};
+
+const fetchProfileFailed = (error) => {
+  return {
+    type: reduxHelper(FETCH_USER_INFO).actionFailure,
+    payload: error,
+  };
+};
+
+export {fetchProfileRequest, fetchProfileSuccess, fetchProfileFailed};
---
+++ src/redux/reducers/profile.reducer.js
@@ -0,0 +1,34 @@
+import {FETCH_USER_INFO} from 'redux/actions/actionTypes';
+import {reduxHelper} from 'redux/utils/reduxHelper';
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  error: '',
+};
+
+const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case reduxHelper(FETCH_USER_INFO).actionRequest:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case reduxHelper(FETCH_USER_INFO).actionSuccess:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+      };
+    case reduxHelper(FETCH_USER_INFO).actionFailure:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default profileReducer;
